b444d7c88074cce702eb2dae2ea161bd85587cd9
Kinga Milaszkiewicz <kinga.milaszkiewicz@outlook.com>
2019-10-28 15:26:39 +0100

    Added an add button

--- Application.py
+++ Application.py
@@ -1,5 +1,6 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QLabel, QGridLayout, QDesktopWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QLabel, QGridLayout, QDesktopWidget, QPushButton
+from PyQt5.QtCore import pyqtSlot
 
 class Application(QWidget):
     def __init__(self):
@@ -10,7 +11,19 @@ class Application(QWidget):
         centerPoint = QDesktopWidget().availableGeometry().center()
         qtRectangle.moveCenter(centerPoint)
         self.move(qtRectangle.topLeft())
+        self.initUI()
+
+    def initUI(self):
+        button = QPushButton("Add", self)
+        button.setToolTip("Adds a record to the password database")
+        button.move(15, 10)
+        button.clicked.connect(self.on_click)
+
         self.show()
+
+    @pyqtSlot()
+    def on_click(self):
+        print("Added a record!")
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
